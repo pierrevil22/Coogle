67cfdf4110a3cb507a16bd06e7e9948d81cddbf3
Carden Pierre-Vil <pierrevil22@yahoo.com>
2020-03-24 11:06:24 -0400

    created index page, css style sheet to style the page, and add the logo

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Welcome to Coogle</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+    
+   
+   
+    <div class="wrapper indexPage">
+   
+   
+        <div class="mainSection">
+
+            <div class="logoContainer">
+                <img src="assets/images/coogleLogo.png">
+            </div>
+
+        
+            <div class="searchContainer">
+
+                <form action="search.php" method="GET">
+                
+                    <input class="searchBox" type="text" name="term">
+                    <input class="searchButton" type="submit" name="Search">
+            
+            
+                </form>
+        
+            </div>
+
+
+        </div>
+
+
+    </div>
+
+</body>
+</html>
