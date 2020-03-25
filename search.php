cafd7e8cf7200570c5467c2a6f344e53720a4ad9
Carden Pierre-Vil <pierrevil22@yahoo.com>
2020-03-25 11:48:55 -0400

    added page limits to DB query. Created the pagination control images and have the number of pages showing.

--- search.php
+++ search.php
@@ -10,6 +10,7 @@ else {
 }
 
 $type = isset($_GET["type"]) ? $_GET["type"] : "sites";
+$page = isset($_GET["page"]) ? $_GET["page"] : 1;
 
 
 	
@@ -91,6 +92,7 @@ $type = isset($_GET["type"]) ? $_GET["type"] : "sites";
 
 			<?php
 			$resultsProvider = new SiteResultsProvider($con);
+			$pageSize = 20;
 
 			$numResults = $resultsProvider->getNumResults($term);
 
@@ -98,11 +100,85 @@ $type = isset($_GET["type"]) ? $_GET["type"] : "sites";
 
 
 
-			echo $resultsProvider->getResultsHtml(1, 20, $term);
+			echo $resultsProvider->getResultsHtml($page, $pageSize, $term);
 			?>
 
 
 		</div>
+
+
+
+		<div class="paginationContainer">
+
+			<div class="pageButtons">
+
+
+
+				<div class="pageNumberContainer">
+					<img src="assets/images/pageStart.png">
+				</div>
+
+				<?php
+
+				$pagesToShow = 10;
+				$numPages = ceil($numResults / $pageSize);
+				$pagesLeft = min($pagesToShow, $numPages);
+
+				$currentPage = $page - floor($pagesToShow / 2);
+
+				if($currentPage < 1) {
+					$currentPage = 1;
+				}
+
+				if($currentPage + $pagesLeft > $numPages + 1) {
+					$currentPage = $numPages + 1 - $pagesLeft;
+				}
+
+				while($pagesLeft != 0 && $currentPage <= $numPages) {
+
+					if($currentPage == $page) {
+						echo "<div class='pageNumberContainer'>
+								<img src='assets/images/pageSelected.png'>
+								<span class='pageNumber'>$currentPage</span>
+							</div>";
+					}
+					else {
+						echo "<div class='pageNumberContainer'>
+								<a href='search.php?term=$term&type=$type&page=$currentPage'>
+									<img src='assets/images/page.png'>
+									<span class='pageNumber'>$currentPage</span>
+								</a>
+						</div>";
+					}
+
+
+					$currentPage++;
+					$pagesLeft--;
+
+				}
+
+
+
+
+
+				?>
+
+				<div class="pageNumberContainer">
+					<img src="assets/images/pageEnd.png">
+				</div>
+
+
+
+			</div>
+
+
+
+
+		</div>
+
+
+
+
 
 
 
