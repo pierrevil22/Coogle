cde088e6f17c58a4237c8454c58b9aa0d02f2bb7
Carden Pierre-Vil <pierrevil22@yahoo.com>
2020-03-25 09:40:15 -0400

    styled the number of results and search results. Trimming the search results, and showing search term in the text box.

--- search.php
+++ search.php
@@ -1,84 +1,112 @@
 <?php
 include("config.php");
+include("classes/SiteResultsProvider.php");
 
-    if(isset($_GET["term"])) {
-        $term = $_GET["term"];
-    }
-    else {
-        exit("You must enter a search term");
-    }
+if(isset($_GET["term"])) {
+	$term = $_GET["term"];
+}
+else {
+	exit("You must enter a search term");
+}
 
-    $type = isset($_GET["type"]) ? $_GET["type"] : "sites";
+$type = isset($_GET["type"]) ? $_GET["type"] : "sites";
 
 
-
+	
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Welcome to Coogle</title>
-    
-    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<title>Welcome to Doodle</title>
+
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
 
 </head>
 <body>
-    
-   <div class="wrapper">
-   
-        <div class ="header">
+
+	<div class="wrapper">
+	
+		<div class="header">
 
 
-            <div class="headerContent">
+			<div class="headerContent">
 
-                <div class="logoContainer">
-                    <a href="index.php">
-                        <img src="assets/images/coogleLogo.png">
-                    </a>
-                </div>
+				<div class="logoContainer">
+					<a href="index.php">
+						<img src="assets/images/doodleLogo.png">
+					</a>
+				</div>
 
-                <div class="searchContainer">
+				<div class="searchContainer">
 
-                    <form action="search.php" method="GET">
+					<form action="search.php" method="GET">
 
-                        <div class="searchBarContainer">
+						<div class="searchBarContainer">
 
-                            <input class="searchBox" type="text" name="term">
-                            <button class="searchButton">
-                                <img src="assets/images/icons/search.png">
-                            </button>                    
-                        </div>
+							<input class="searchBox" type="text" name="term" value="<?php echo $term; ?>">
+							<button class="searchButton">
+								<img src="assets/images/icons/search.png">
+							</button>
+						</div>
 
-                    </form>
+					</form>
 
-                </div>
+				</div>
 
-            </div>
-       
-       
-        <div class="tabsContainer">
-
-            <ul class="tabList">
-                
-                <li class="<?php echo $type == 'sites' ? 'active' : '' ?>">
-                    <a href='<?php echo "search.php?term=$term&type=sites"; ?>'>
-                        Sites
-                    </a>
-                </li>
-
-                <li class="<?php echo $type == 'images' ? 'active' : '' ?>">
-                    <a href='<?php echo "search.php?term=$term&type=images"; ?>'>
-                        Images 
-                    </a>
-                </li>
+			</div>
 
 
+			<div class="tabsContainer">
+
+				<ul class="tabList">
+
+					<li class="<?php echo $type == 'sites' ? 'active' : '' ?>">
+						<a href='<?php echo "search.php?term=$term&type=sites"; ?>'>
+							Sites
+						</a>
+					</li>
+
+					<li class="<?php echo $type == 'images' ? 'active' : '' ?>">
+						<a href='<?php echo "search.php?term=$term&type=images"; ?>'>
+							Images
+						</a>
+					</li>
+
+				</ul>
 
 
-        </div>
-        </div>
-          
+			</div>
+		</div>
 
-    </div>
+
+
+
+
+
+
+
+
+
+		<div class="mainResultsSection">
+
+			<?php
+			$resultsProvider = new SiteResultsProvider($con);
+
+			$numResults = $resultsProvider->getNumResults($term);
+
+			echo "<p class='resultsCount'>$numResults results found</p>";
+
+
+
+			echo $resultsProvider->getResultsHtml(1, 20, $term);
+			?>
+
+
+		</div>
+
+
+
+	</div>
 
 </body>
 </html>
