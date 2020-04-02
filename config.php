ef35311ad009e840fdf371093bca1fc3adc1666f
Carden Pierre-Vil <pierrevil22@yahoo.com>
2020-04-02 12:55:45 -0400

    added composer file and update config

--- config.php
+++ config.php
@@ -9,5 +9,38 @@ try {
 catch(PDOExeption $e) {
 	echo "Connection failed: " . $e->getMessage();
 }
+
+//Get Heroku ClearDB connection information
+$cleardb_url      = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server   = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db       = substr($cleardb_url["path"],1);
+
+
+$active_group = 'default';
+$query_builder = TRUE;
+
+$db['default'] = array(
+    'dsn'    => '',
+    'hostname' => 'us-cdbr-iron-east-01.cleardb.net:3306',
+    'username' => 'b32bdfcf89ec70',
+    'password' => '9edd9180',
+    'database' => $cleardb_db,
+    'dbdriver' => 'mysqli',
+    'dbprefix' => '',
+    'pconnect' => FALSE,
+    'db_debug' => (ENVIRONMENT !== 'production'),
+    'cache_on' => FALSE,
+    'cachedir' => '',
+    'char_set' => 'utf8',
+    'dbcollat' => 'utf8_general_ci',
+    'swap_pre' => '',
+    'encrypt' => FALSE,
+    'compress' => FALSE,
+    'stricton' => FALSE,
+    'failover' => array(),
+    'save_queries' => TRUE
+);
 ?>
 
