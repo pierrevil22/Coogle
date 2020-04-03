d73986316e9697c60b2f62f5f46becfa2f6c5040
Carden Pierre-Vil <pierrevil22@yahoo.com>
2020-04-02 22:56:52 -0400

    new logic

--- config.php
+++ config.php
@@ -11,4 +11,36 @@ catch(PDOExeption $e) {
 }
 
 
+//Get Heroku ClearDB connection information
+$cleardb_url      = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server   = $cleardb_url["us-cdbr-iron-east-01.cleardb.net"];
+$cleardb_username = $cleardb_url["b32bdfcf89ec70"];
+$cleardb_password = $cleardb_url["9edd9180"];
+$cleardb_db       = substr($cleardb_url["https://git.heroku.com/coogle.git"],1);
+
+
+$active_group = 'default';
+$query_builder = TRUE;
+
+$db['default'] = array(
+    'dsn'    => '',
+    'hostname' => $cleardb_server,
+    'username' => $cleardb_username,
+    'password' => $cleardb_password,
+    'database' => $cleardb_db,
+    'dbdriver' => 'mysqli',
+    'dbprefix' => '',
+    'pconnect' => FALSE,
+    'db_debug' => (ENVIRONMENT !== 'production'),
+    'cache_on' => FALSE,
+    'cachedir' => '',
+    'char_set' => 'utf8',
+    'dbcollat' => 'utf8_general_ci',
+    'swap_pre' => '',
+    'encrypt' => FALSE,
+    'compress' => FALSE,
+    'stricton' => FALSE,
+    'failover' => array(),
+    'save_queries' => TRUE
+);
 ?>
