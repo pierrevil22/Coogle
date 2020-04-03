16c744274c254e97f43ce1e5a88ebab8b3327464
Carden Pierre-Vil <pierrevil22@yahoo.com>
2020-04-03 00:22:22 -0400

    starting over

--- config.php
+++ config.php
@@ -1,41 +1,14 @@
 <?php
+ob_start();
+
+try {
+
+    $con = new PDO("mysql:dbname=coogle;host=localhost", "root", "");
+    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+}
+catch(PDOExeption $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
 
 
-
-
-//Get Heroku ClearDB connection information
-$cleardb_url      = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server   = $cleardb_url["us-cdbr-iron-east-01.cleardb.net"];
-$cleardb_username = $cleardb_url["b32bdfcf89ec70"];
-$cleardb_password = $cleardb_url["9edd9180"];
-$cleardb_db       = substr($cleardb_url["https://git.heroku.com/coogle.git"],1);
-
-
-
-
-
-$active_group = 'default';
-$query_builder = TRUE;
-
-$db['default'] = array(
-    'dsn'    => '',
-    'hostname' => 'us-cdbr-iron-east-01.cleardb.net',
-    'username' => 'b32bdfcf89ec70',
-    'password' => '9edd9180',
-    'database' => 'heroku_01494fa3d0a4192',
-    'dbdriver' => 'mysqli',
-    'dbprefix' => '',
-    'pconnect' => FALSE,
-    'db_debug' => (ENVIRONMENT !== 'production'),
-    'cache_on' => FALSE,
-    'cachedir' => '',
-    'char_set' => 'utf8',
-    'dbcollat' => 'utf8_general_ci',
-    'swap_pre' => '',
-    'encrypt' => FALSE,
-    'compress' => FALSE,
-    'stricton' => FALSE,
-    'failover' => array(),
-    'save_queries' => TRUE
-);
 ?>
