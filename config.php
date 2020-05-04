4332207df68c40078baad6f54243f7f01e19d9ad
Carden Pierre-Vil <pierrevil22@yahoo.com>
2020-05-04 14:44:40 -0400

    trying combined method

--- config.php
+++ config.php
@@ -8,18 +8,23 @@ try {
 
 //New Sequel Pro to connect to ClearDB
 
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
+// $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+// $server = $url["host"];
+// $username = $url["user"];
+// $password = $url["pass"];
+// $db = substr($url["path"], 1);
 
-$config = array(
-    'host' => "us-cdbr-iron-east-01.cleardb.net" ,
-    'user' => "b32bdfcf89ec70" ,
-    'pw' => "9edd9180",
-    'db' => "heroku_01494fa3d0a4192" 
-);
+// $config = array(
+//     'host' => "us-cdbr-iron-east-01.cleardb.net" ,
+//     'user' => "b32bdfcf89ec70" ,
+//     'pw' => "9edd9180",
+//     'db' => "heroku_01494fa3d0a4192" 
+// );
+
+// combined methods 
+
+    $con = new PDO("mysql:dbname=heroku_01494fa3d0a4192;host=us-cdbr-iron-east-01.cleardb.net", "b32bdfcf89ec70", "9edd9180");
+    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 
 
 
